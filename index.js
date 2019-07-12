14a8e090266b9560bde6f41ccb0ae3d668891e91
Thikamporn Simud <thikamporn.s@ku.th>
2019-07-12 09:48:26 +0700

    listen port

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+require('dotenv').config()
+// const line = require('@line/bot-sdk')
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const config = require('./config.js');
+// const webhookHandler = require('./handlers/webhookHandler.js');
+
+// listen on port
+const port = process.env.PORT || 4000
+
+// create Express app
+const app = express();
+
+// register a webhook handler with middleware
+app.post('/webhook', (req, res) => res.sendStatus(200))
+// app.post('/webhook', line.middleware(config), webhookHandler);
+
+app.listen(port)
