d7c4b358718393637a612566ad4fa5fca76bf0f9
Thikamporn Simud <thikamporn.s@ku.th>
2019-07-17 11:20:20 +0700

    test

--- index.js
+++ index.js
@@ -20,29 +20,40 @@ app.get('/', (req, res) => {
 })
 
 app.post('/webhook', (req, res) => {
-    if (req.body) {
+    if (req.body.queryResult.parameters.action == "BMI.BMI-custom.BMI-custom-yes"
+        && req.body.queryResult.parameters) {
+            res.send({
+                //Test get value of WebhookClient
+                bodysuccess: true,
+                agentVersion: agent.agentVersion,
+                intent: agent.inten,
+                locale: agent.locale,
+                query: agent.query,
+                session: agent.session,
+            });
+        // var speech = "999999999";
+    }
         //Create an instance
         const agent = new WebhookClient({request: req, response: res});
-        res.send({
-            //Test get value of WebhookClient
-            bodysuccess: true,
-            agentVersion: agent.agentVersion,
-            intent: agent.inten,
-            locale: agent.locale,
-            query: agent.query,
-            session: agent.session,
-        });
+        // res.send({
+        //     //Test get value of WebhookClient
+        //     bodysuccess: true,
+        //     agentVersion: agent.agentVersion,
+        //     intent: agent.inten,
+        //     locale: agent.locale,
+        //     query: agent.query,
+        //     session: agent.session,
+        // });
 
-        function stringTest(agent) {
-            agent.add('My name is thikamporn');
-        }
+        // function stringTest(agent) {
+        //     agent.add('My name is thikamporn');
+        // }
         // console.log(req.body);
         
         // Run the proper function handler based on the matched Dialogflow intent name
-        let intentMap = new Map();
-        intentMap.set('BMI - custom - yes', stringTest);
-        agent.handleRequest(intentMap);
-    }
+        // let intentMap = new Map();
+        // intentMap.set('BMI - custom - yes', stringTest);
+        // agent.handleRequest(intentMap);
     
     res.end()
   });
@@ -54,36 +65,36 @@ app.post('/webhook', (req, res) => {
   app.listen(port, () => {
     console.log(`Server is running at port: ${port}`);
   })
-// app.post("/webhook", (req, res) => {
-//    if (req.body.queryResult.parameters.account_information == "contact number"
-//     && req.body.queryResult.parameters.account_information) {
-//     var speech = "999999999";
-//   }
-//   else if (req.body.queryResult.parameters.account_information == "account number"
-//     && req.body.queryResult.parameters.account_information) {
-//     var speech = "9999999999999";
-//   }
-//   else if (req.body.queryResult.parameters.account_information == "DOB"
-//     && req.body.queryResult.parameters.account_information) {
-//     var speech = "1 Jan 2019";
-//   }
-//   else if (req.body.queryResult.parameters.account_information == "address"
-//     && req.body.queryResult.parameters.account_information) {
-//     var speech = " floor no 1 , Building no 1 , address";
-//   }
-//   return res.json({
-//     fulfillmentText: "fulfillmentText",
-//     fulfillmentMessages: [{
-//       simpleResponses: {
-//         simpleResponses: [{
-//           "textToSpeech": "textToSpeech",
-//           "displayText": speech
-//         }]
-//       }
-//     }],
-//     source: "webhook-sample"
-//   });
-// });
+app.post("/webhook", (req, res) => {
+   if (req.body.queryResult.parameters.account_information == "contact number"
+    && req.body.queryResult.parameters.account_information) {
+    var speech = "999999999";
+  }
+  else if (req.body.queryResult.parameters.account_information == "account number"
+    && req.body.queryResult.parameters.account_information) {
+    var speech = "9999999999999";
+  }
+  else if (req.body.queryResult.parameters.account_information == "DOB"
+    && req.body.queryResult.parameters.account_information) {
+    var speech = "1 Jan 2019";
+  }
+  else if (req.body.queryResult.parameters.account_information == "address"
+    && req.body.queryResult.parameters.account_information) {
+    var speech = " floor no 1 , Building no 1 , address";
+  }
+  return res.json({
+    fulfillmentText: "fulfillmentText",
+    fulfillmentMessages: [{
+      simpleResponses: {
+        simpleResponses: [{
+          "textToSpeech": "textToSpeech",
+          "displayText": speech
+        }]
+      }
+    }],
+    source: "webhook-sample"
+  });
+});
 
 // app.get('/webhook', function (req, res) {
 //     res.send('hello webhook');
