22fd89ac4f622cd6dc296ed52419f8f4bfaecb81
Thikamporn Simud <thikamporn.s@ku.th>
2019-07-17 11:21:17 +0700

    test

--- index.js
+++ index.js
@@ -62,39 +62,39 @@ app.post('/webhook', (req, res) => {
 //     agent.add(`My name is thikamporn`);
 //   }
 
-  app.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server is running at port: ${port}`);
-  })
-app.post("/webhook", (req, res) => {
-   if (req.body.queryResult.parameters.account_information == "contact number"
-    && req.body.queryResult.parameters.account_information) {
-    var speech = "999999999";
-  }
-  else if (req.body.queryResult.parameters.account_information == "account number"
-    && req.body.queryResult.parameters.account_information) {
-    var speech = "9999999999999";
-  }
-  else if (req.body.queryResult.parameters.account_information == "DOB"
-    && req.body.queryResult.parameters.account_information) {
-    var speech = "1 Jan 2019";
-  }
-  else if (req.body.queryResult.parameters.account_information == "address"
-    && req.body.queryResult.parameters.account_information) {
-    var speech = " floor no 1 , Building no 1 , address";
-  }
-  return res.json({
-    fulfillmentText: "fulfillmentText",
-    fulfillmentMessages: [{
-      simpleResponses: {
-        simpleResponses: [{
-          "textToSpeech": "textToSpeech",
-          "displayText": speech
-        }]
-      }
-    }],
-    source: "webhook-sample"
-  });
-});
+})
+// app.post("/webhook", (req, res) => {
+//    if (req.body.queryResult.parameters.account_information == "contact number"
+//     && req.body.queryResult.parameters.account_information) {
+//     var speech = "999999999";
+//   }
+//   else if (req.body.queryResult.parameters.account_information == "account number"
+//     && req.body.queryResult.parameters.account_information) {
+//     var speech = "9999999999999";
+//   }
+//   else if (req.body.queryResult.parameters.account_information == "DOB"
+//     && req.body.queryResult.parameters.account_information) {
+//     var speech = "1 Jan 2019";
+//   }
+//   else if (req.body.queryResult.parameters.account_information == "address"
+//     && req.body.queryResult.parameters.account_information) {
+//     var speech = " floor no 1 , Building no 1 , address";
+//   }
+//   return res.json({
+//     fulfillmentText: "fulfillmentText",
+//     fulfillmentMessages: [{
+//       simpleResponses: {
+//         simpleResponses: [{
+//           "textToSpeech": "textToSpeech",
+//           "displayText": speech
+//         }]
+//       }
+//     }],
+//     source: "webhook-sample"
+//   });
+// });
 
 // app.get('/webhook', function (req, res) {
 //     res.send('hello webhook');
