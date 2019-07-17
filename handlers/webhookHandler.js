1a3b8af1687f100cfcfaac71d044bd60d7dd88ce
Thikamporn Simud <thikamporn.s@ku.th>
2019-07-17 23:05:57 +0700

    test

--- handlers/webhookHandler.js
+++ handlers/webhookHandler.js
@@ -76,18 +76,4 @@ const webhook = (req, res) => {
     res.end()
 }
 
-// event handler
-// function handleEvent(event) {
-//     if (event.type !== 'message' || event.message.type !== 'text') {
-//       // ignore non-text-message event
-//       return Promise.resolve(null);
-//     }
-  
-//     // create a echoing text message
-//     const echo = { type: 'text', text: event.message.text };
-  
-//     // use reply API
-//     return client.replyMessage(event.replyToken, echo);
-// }
-
 module.exports = webhook;
