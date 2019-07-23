5c0c89565ad496eb109ec6724ccd5e617000ec47
Thikamporn Simud <thikamporn.s@ku.th>
2019-07-23 10:35:55 +0700

    test db connect

--- handlers/DBHandler.js
+++ handlers/DBHandler.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 // const DBconfig = require('./DBconfig.js')
 var db = mysql.createConnection({
     host : process.env.DB_HOST,
-    port : process.env.PORT,
+    // port : process.env.PORT,
     user : process.env.DB_USER,
     password : process.env.DB_PWD,
     database : process.env.DB_NAME
